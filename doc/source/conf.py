a2d4779f84e9012cb9a130b5faacf8614b791266
Ta-Shun Su <antoniost29@gmail.com>
2024-01-17 11:32:12 +0100

    save wiki module

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -62,3 +62,7 @@ autodoc_class_signature = 'separated'
 autodoc_typehints = 'description'
 autodoc_typehints_format = 'short'
 autodoc_inherit_docstrings = True
+
+# -- Options for sphinx-markdown-builder -------------------------------------
+
+markdown_uri_doc_suffix = ''
