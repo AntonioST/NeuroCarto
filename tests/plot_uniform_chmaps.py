01adbea611a42d7d6d6ab929286213d5bef714ba
Ta-Shun Su <antoniost29@gmail.com>
2024-01-22 18:35:19 +0100

    add plotting script

---
+++ tests/plot_uniform_chmaps.py
@@ -0,0 +1,43 @@
+import sys
+
+import matplotlib
+from matplotlib import pyplot as plt
+
+from chmap.probe_npx.plot import plot_channelmap_block, plot_probe_shape
+from chmap.probe_npx.utils import *
+
+rc = matplotlib.rc_params_from_file('tests/default.matplotlibrc', fail_on_error=True, use_default_template=True)
+plt.rcParams.update(rc)
+
+h1 = npx24_half_density(0)
+h2 = npx24_half_density((0, 1))
+q2 = npx24_quarter_density((0, 1))
+q4 = npx24_quarter_density(None)
+o1 = npx24_one_eighth_density()
+
+chmaps = [h1, h2, q2, q4, o1]
+
+figsize_w, figsize_h = rc['figure.figsize']
+
+fg, ax = plt.subplots(
+    ncols=len(chmaps),
+    figsize=(2 * len(chmaps), figsize_h),
+    sharey='all',
+    sharex='all',
+    squeeze=True
+)
+
+height = 6
+for i, chmap in enumerate(chmaps):
+    plot_channelmap_block(ax[i], chmap, height=height, color='k', shank_width_scale=2)
+    plot_probe_shape(ax[i], chmap.probe_type, height=height, color='gray', label_axis=True, shank_width_scale=2)
+
+    if i != 0:
+        ax[i].set_ylabel(None)
+    ax[i].set_xlabel(None)
+    ax[i].set_xticklabels([])
+
+if len(sys.argv) == 1:
+    plt.show()
+else:
+    plt.savefig(sys.argv[1])
